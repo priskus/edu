0b4ee09fc00b91e7348ea90d1628459bc8b3e366
Max Popov <79455523+priskus@users.noreply.github.com>
2022-02-18 00:06:29 +0300

    mend sefety

--- algorithms/buble_sort/main.cpp
+++ algorithms/buble_sort/main.cpp
@@ -1,32 +1,41 @@
 #include <iostream>
-#include <ctime>
-
-void sort(int *l, int *r)
-{
-    int lg = r - l;
-    if(lg<=1)        return;
-
-    bool flag = true;
-    while(flag){
-        flag = false;
-        for(int *i = l; i < r; ++i)
-        {
-            if(*i > *(i+1)) {
-                std::swap(*i, *(i + 1));
-                flag = true;
-            }
-        }
-        --r;
-    }
-}
-
-int main() {
-    srand (time(NULL));
-    std::cout <<typeid(150).name();
-    int m[10];
-    for(int &n : m){
-        n = rand()%11;
-    }
-    sort(m,m+10);
-    for(int n : m) std::cout << n<<" ";
-    }
+using namespace std;
+  
+void swap(int *xp, int *yp) 
+{ 
+    int temp = *xp; 
+    *xp = *yp; 
+    *yp = temp; 
+} 
+  
+// A function to implement bubble sort 
+void bubbleSort(int arr[], int n) 
+{ 
+    int i, j; 
+    for (i = 0; i < n-1; i++)     
+      
+    // Last i elements are already in place 
+    for (j = 0; j < n-i-1; j++) 
+        if (arr[j] > arr[j+1]) 
+            swap(&arr[j], &arr[j+1]); 
+} 
+  
+/* Function to print an array */
+void printArray(int arr[], int size) 
+{ 
+    int i; 
+    for (i = 0; i < size; i++) 
+        cout << arr[i] << " "; 
+    cout << endl; 
+} 
+  
+// Driver code 
+int main() 
+{ 
+    int arr[] = {64, 34, 25, 12, 22, 11, 90}; 
+    int n = sizeof(arr)/sizeof(arr[0]); 
+    bubbleSort(arr, n); 
+    cout<<"Sorted array: \n"; 
+    printArray(arr, n); 
+    return 0; 
+} 
